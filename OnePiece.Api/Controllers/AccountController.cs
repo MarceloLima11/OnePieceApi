a74dafd92f34da7fdcb73a9ad49bfc810db38846
Marcelo_Lima <marcelolima152021@gmail.com>
2022-08-04 12:01:33 -0300

    AccountController implementado -> gerar token

---
+++ OnePiece.Api/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnePiece.Application.DTOs;
+
+namespace OnePiece.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[Controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager; 
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "AccountController :: Acessado em : "
+                + DateTime.Now.ToLongDateString();
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO model)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState.Values.SelectMany(e => e.Errors));
+            //}
+
+            var user = new IdentityUser 
+            { 
+                UserName = model.Email,
+                Email = model.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok(/*GeraToken(model)*/);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO userInfo)
+        {
+            // verifica as credenciais do usuário e retorna um valor
+            var result = await _signInManager.PasswordSignInAsync(userInfo.Email,
+                userInfo.Password, isPersistent: false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login Inválido....");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
